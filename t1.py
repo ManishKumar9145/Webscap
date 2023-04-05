2688a851df90aeb6c5a9436ea6bff82f62c4c971
ManishKumar9145 <110729007+ManishKumar9145@users.noreply.github.com>
2023-04-05 20:48:10 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -2,9 +2,9 @@ import requests #importing requests module
 from bs4 import BeautifulSoup #importing Beautiful soup module
 from csv import writer #from csv importing writer
 
-res=requests.get('https://www.amazon.in/s?rh=n%3A6612025031&fs=true&ref=lp_6612025031_sar') #rquest to get yhe given web page
-soup=BeautifulSoup(res.content,'html.parser') #getting html code of the given web page and contet of the same
-lists=soup.find_all('div',class_="a-section a-spacing-base") #finding all the element with div tag and class a-section a-spacing-base
+res=requests.get('https://www.amazon.in/s?rh=n%3A6612025031&fs=true&ref=lp_6612025031_sar')   #rquest to get yhe given web page
+soup=BeautifulSoup(res.content,'html.parser')   #getting html code of the given web page and contet of the same
+lists=soup.find_all('div',class_="a-section a-spacing-base")  #finding all the element with div tag and class a-section a-spacing-base
 
 with open('amazon.csv','w',encoding='utf8',newline='') as x: #to write the csv file
     thewriter = writer(x)
